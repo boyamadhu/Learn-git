f164fd0412399693b5e0731e38d3c33a2ff2e830
boyamadhu <boyamadhu9874@gmail.com>
2024-08-17 16:44:47 +0530

    added code in prime_programme.py

--- prime_programme.py
+++ prime_programme.py
@@ -1,1 +1,8 @@
-
+def prime(num):
+    if num<=1:
+        return False
+    for i in range(2,num//2+1):
+        if num%i==0:
+            return False
+    return True
+print(prime(int(input('Please Enter the number :'))))
